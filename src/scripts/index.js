f4aa4cd6bffef3de2337ef30834a76eff5ee3e27
GarrySh <sooo.lame@gmail.com>
2019-02-10 20:44:32 +1000

    draw in canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,35 @@ canvas.height = document.body.clientHeight;
 
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(200,0,0)';
-ctx.fillRect(10, 10, 55, 50);
+ctx.strokeStyle = 'green';
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30, 30, 55, 50);
+const draw = event => {
+  if (!isDrawing) {
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+};
+
+canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('mousedown', event => {
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener('mouseout', () => {
+  isDrawing = false;
+});
